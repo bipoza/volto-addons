1a2c5e92b1a8b8ee6e747e332dc110d831eb37c5
bipoza <bipoza@gmail.com>
2021-05-12 20:26:03 +0200

    git add timestamp

--- scripts/cache_generator.js
+++ scripts/cache_generator.js
@@ -1,17 +1,18 @@
 const {getAwesomeVoltoToJSON} = require('./addons_md_scrapper');
 const {getAddonsMoreInfo} = require('./get_addons_more_info');
-
+const {getTimestamp} = require('./utils');
 function generateCache() {
     getAwesomeVoltoToJSON().then(json => {
-        getAddonsMoreInfo(json).then(res=>{
-console.log("RES: ", res)
-        })
+        // getAddonsMoreInfo(json).then(res=>{
+// console.log("RES: ", res)
+        // })
         var fs = require('fs');
-        fs.writeFile("./cache.json", JSON.stringify(json, null, 4), (err) => {
+        fs.writeFile("./cache.json", JSON.stringify({last_update:getTimestamp(), data:json}, null, 4), (err) => {
             if (err) {  console.error(err);  return; };
             console.log("File has been created");
         });
-    })
+    // })
+});
 }
 
 
